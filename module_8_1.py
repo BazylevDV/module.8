b1c0a347137d0d6e06abd9931e5275772d97ce95
BazylevDV <dmbazylev@gmail.com>
2024-09-06 15:22:08 +0500

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,14 @@
+def add_everything_up(a, b):
+    if isinstance(a, (int, float)) and isinstance(b, str):
+        return str(a) + b
+    elif isinstance(a, str) and isinstance(b, (int, float)):
+        return a + str(b)
+    elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a + b
+    else:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))  # Ожидаемый результат: '123.456строка'
+print(add_everything_up('яблоко', 4215))  # Ожидаемый результат: 'яблоко4215'
+print(add_everything_up(123.456, 7))  # Ожидаемый результат: 130.456
